356c373034b61ed232e90c8e5f137fba6bfe72ed
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-05 22:44:52 -0300

    fim da aula de template string

--- webModernoCurso/strings.js
+++ webModernoCurso/strings.js
@@ -1,4 +1,4 @@
-const escola = 'cod3r'
+const escola = 'Cod3r'
 
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
@@ -9,6 +9,8 @@ console.log(escola.substring(1))
 console.log(escola.substring(0,3))
 
 console.log('Escola '.concat(escola).concat('!'))
+console.log('Escola ' +  escola  + "!")
+console.log('3' + 2 )
 console.log(escola.replace(3 ,'e'))
 console.log(escola.replace(/\d/, 'e'))
 console.log(escola.replace(/\w/g, 'e'))
