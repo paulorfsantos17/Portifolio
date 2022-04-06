4c2b784349e358db93b3339a172c34ddecf23799
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-06 15:40:05 -0300

    logicos aula

---
+++ webModernoCurso/logicos.js
@@ -0,0 +1,16 @@
+function compras(trabalho1 , trabalho2){
+        const comprarSorvete = trabalho1 || trabalho2
+        const comprartv50 = trabalho1 && trabalho2     
+        //const comprartv32 = !!(trabalho1 ^ trabalho2) //bitwise xor 
+        const comprartv32= trabalho1 =!  trabalho2
+        const manterSaudavel = !comprarSorvete
+
+        return {
+            comprarSorvete,
+            comprartv50, 
+            comprartv32, 
+            manterSaudavel
+        }
+}
+
+console.log(compras(true , false))
