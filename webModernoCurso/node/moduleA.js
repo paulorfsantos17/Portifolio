59f28c65e784a9a7140b6ae0a69d9fd8466ee46a
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-12 15:41:11 -0300

    gitignore

---
+++ webModernoCurso/node/moduleA.js
@@ -0,0 +1,5 @@
+this.ola = "Olá Pessoal"
+
+exports.bemVindo = "Bem vindo ao node"
+
+module.exports.ateLogo = "Até o proximo exemplo!"
---
+++ webModernoCurso/node/moduloCliente.js
@@ -0,0 +1,12 @@
+const moduloA = require('./moduleA')
+const modulob = require('./ModuleB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+
+console.log(moduloA)
+
+console.log(modulob.bomdia)
+console.log(modulob.boanoite())
+console.log(modulob)
