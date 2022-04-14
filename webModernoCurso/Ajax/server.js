b3c6e0bee38d792b5c7e2a027eb8cb663bfe0df8
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-14 17:28:38 -0300

    xmlHttpRequest1.html concluido

--- webModernoCurso/Ajax/server.js
+++ webModernoCurso/Ajax/server.js
@@ -1,10 +1,20 @@
 const bodyparser = require('body-parser')
 const express = require('express')
 const app = express()
+const multer = require('multer')
 
 app.use(express.static('.'))
 app.use(bodyparser.urlencoded({extended : true}))
 app.use(bodyparser.json())
+
+const storage = multer.diskStorage({
+    destination : function(req , file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req , file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
 
 app.get('/teste', (req, res) => res.send('ok'))
 app.listen(8080,() => console.log('Executando'))
