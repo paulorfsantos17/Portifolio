81115e6ed1bf654e7cac4ca143fdb8d0066b6329
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-15 20:19:14 -0300

    fetch fim

--- webModernoCurso/Ajax/server.js
+++ webModernoCurso/Ajax/server.js
@@ -28,5 +28,12 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+        res.send({
+            ...req.body,
+            id: 1
+        })
+})
+
 app.get('/teste', (req, res) => res.send('ok'))
 app.listen(8080,() => console.log('Executando'))
