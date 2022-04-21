6384c6d831cd64945d1fe06ba7e7d7cc528cfc44
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-21 18:32:04 -0300

    Inicio do modo de crud

---
+++ webModernoCurso/react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Main.css'
+import Header from './Header'
+
+export default props => 
+    <React.Fragment>
+        <Header />
+        <main className="container">
+
+        </main>
+    </React.Fragment>
