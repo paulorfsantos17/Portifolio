dd830cc54f4bf6dd06feaae9c0acd71604e9a8e9
Paulo Santos <paulorfsantos17@gmail.com>
2022-04-30 00:21:19 -0300

    inicio do component tabela

---
+++ webModernoCurso/nextJS/next-crud/src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import Tittle from "./Tittle";
+
+
+interface LayoutProps {
+    tittle : string
+    children: any
+
+}
+
+export default function Layout(props : LayoutProps) {
+    
+    return(
+        <div className={`
+            flex flex-col w-2/3
+            bg-white text-gray-800
+            rounded-md
+        `}>
+            <Tittle>{props.tittle}</Tittle>
+            <div className="p-6">
+                {props.children}
+            </div>
+
+        </div>
+    )
+
+}
---
+++ webModernoCurso/nextJS/next-crud/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout"
+
+export default function Home() {
+  return (
+    <div className={`flex justify-center items-center h-screen
+         bg-gradient-to-r from-blue-500 to-purple-500
+         text-white
+    `}>
+        <Layout tittle="Cadastro Simples">
+          <span>Conteudo</span>
+        </Layout>
+    
+
+     </div>
+  )
+}
