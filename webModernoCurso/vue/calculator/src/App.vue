d8903184257c66166c332ba60c704629e1aca909
Paulo Santos <paulorfsantos17@gmail.com>
2022-05-11 21:04:36 -0300

    calculadora refatorada vue fim desing

---
+++ webModernoCurso/vue/calculator/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <display-vue id="app">
+        
+        <ProjectCalculator />
+    </display-vue>
+</template>
+
+<script>
+import ProjectCalculator from './main/Calculator.vue'
+
+export default {
+    components: { ProjectCalculator }
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+*{
+    font-family: "RobotoMono", monospace;
+}
+
+#app{
+    display: flex;
+    flex-direction: "column";
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: white;
+    background: rgb(55,55,55);
+    background: linear-gradient(90deg, rgba(55,55,55,1) 0%, rgba(69,69,69,1) 35%, rgba(167,169,170,1) 100%);
+}
+</style>
---
+++ webModernoCurso/vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="calculator">
+        <h1>Calculator</h1>
+
+        <DisplayCalculator value="1000" />
+        <ButtonCalculator label="AC" triple/>
+        <ButtonCalculator label="/" operation/>
+        <ButtonCalculator label="7"/>
+        <ButtonCalculator label="8"/>
+        <ButtonCalculator label="9"/>
+        <ButtonCalculator label="*"  operation/>
+        <ButtonCalculator label="4"/>
+        <ButtonCalculator label="5"/>
+        <ButtonCalculator label="6"/>
+        <ButtonCalculator label="-" operation/>
+        <ButtonCalculator label="1"/>
+        <ButtonCalculator label="2"/>
+        <ButtonCalculator label="3"/>
+        <ButtonCalculator label="+" operation/>
+        <ButtonCalculator label="0" double/>
+        <ButtonCalculator label="." />
+        <ButtonCalculator label="=" operation />
+    </div>
+</template>
+
+<script>
+import DisplayCalculator from '../components/Display'
+import ButtonCalculator from '../components/Button'
+
+
+export default {
+    name: 'ProjectCalculator',
+    components: {DisplayCalculator , ButtonCalculator}
+
+}
+</script>
+
+<style>
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+    
+</style>
